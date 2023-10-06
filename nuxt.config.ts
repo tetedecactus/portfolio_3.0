48b842a999130711658af9e30dff5025aceeef35
TeteDeCactus <o.labrecque.lacasse@gmail.com>
2023-10-05 21:46:55 -0400

    change node to bun criss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
-    link: [ { rel: 'icon', type: 'image/png', sizes: '24x24', href: '~/programmeur.png' }, ],
+    link: [ { rel: 'icon', type: 'image/png', sizes: '24x24', index: 0, href: '/programmeur.png' }, ],
   },
   site: {
     url: 'https://olablac.com',
